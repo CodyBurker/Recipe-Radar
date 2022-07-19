cc761bf7f494e79b1a417d49038d9175fca27a68
Robert Turnage <Robert.Turnage@berkeley.edu>
2022-07-19 17:49:41 -0500

    Flask update
    Working

--- flaskapp/w209dv22sec6g1/blueprints/Margo_Suryanaga.py
+++ flaskapp/w209dv22sec6g1/blueprints/Margo_Suryanaga.py
@@ -1,34 +1,33 @@
 from flask import Blueprint
 import altair as alt
 import pandas as pd
-import pandasql
 
-Rishika_Pulvender = Blueprint('Rishika_Pulvender', __name__)
+Margo_Suryanaga = Blueprint('Margo_Suryanaga', __name__)
 
 ###########################
 ###### Data Sources #######
 ###########################
 def nutrition_data():
     
-    path = '/data/nutrition.csv'
+    path = '/w209dv22sec6g1/data/flavorprofiles.csv'
     df = pd.read_csv(path)
     return df
 
 ###########################
-####### nutrition #########
+##### flavorprofiles ######
 ###########################
 
-@Rishika_Pulvender.route("/data/nutrition/RPChart")
+@Margo_Suryanaga.route("/data/flavorprofiles/MSChart")
 def chart():
     
     source = pd.DataFrame({
-    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+        'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+        'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
     })
-
+    
     chart = alt.Chart(source).mark_bar().encode(
         x='a',
         y='b'
     )
-    
+        
     return chart.to_json()
